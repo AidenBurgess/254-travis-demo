02023390a793163849403c0e9940e38ef104c3c4
Aiden Burgess <aiden9874@gmail.com>
2020-10-21 10:26:33 +1300

    Add welcome message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
    public static void main(String[] args) {
       int num1 = 16;
       int num2 = 3;
-
+      System.out.println("Hi and welcome to my Travis Scott Collab!");
       System.out.printf("%d + %d = %d%n", num1, num2, TravisMath.add(num1, num2));
       System.out.printf("%d - %d = %d%n", num1, num2, TravisMath.subtract(num1, num2));
       System.out.printf("%d * %d = %d%n", num1, num2, TravisMath.multiply(num1, num2));
