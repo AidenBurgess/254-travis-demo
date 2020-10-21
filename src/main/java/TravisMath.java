f12677192d48a16f881b37100124042ed91f76d1
Aiden Burgess <aiden9874@gmail.com>
2020-10-21 15:15:18 +1300

    Revert "Fix add and subtract methods"
    This reverts commit c9ee7aa10bb44ab5e1baa5dd890a62ac1fffb35f.

--- src/main/java/TravisMath.java
+++ src/main/java/TravisMath.java
@@ -2,11 +2,11 @@ package main.java;
 
 public class TravisMath {
    public static int add(int one, int two) {
-      return one + two;
+      return one - two;
    }
 
    public static int subtract(int one, int two) {
-      return one - two;
+      return one + two;
    }
 
    public static int multiply(int one, int two) {
